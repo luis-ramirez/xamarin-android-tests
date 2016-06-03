a8887f23b7ab8d8ecd8024e7ebac45648d48a571
Yamille Ramirez <yamille@megsoftconsulting.com>
2016-06-02 20:51:37 -0400

    login screen

---
+++ AndroidLogin/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace AndroidLogin
+{
+    [Activity(MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            ActionBar.Hide();
+            
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
